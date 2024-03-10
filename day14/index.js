af9b3fb63d07458741a911ffa1b567771193db88
abrarishere <legendabrar44@gmail.com>
2024-03-10 06:54:52 +0500

    first commit

---
+++ day14/index.js
@@ -0,0 +1,2 @@
+// JavaScript code for Day {day}
+alert("Hello from Day {day}!");
---
+++ day3/index.js
@@ -0,0 +1,11 @@
+let students = 20;
+//students = students + 1;
+//students = students - 1;
+//students = students * 1;
+//students = students / 1;
+//students+=1;
+//students-=1;
+//students*=1;
+//students/=1;
+let results = (1+2)*(3+4)
+console.log(results)
