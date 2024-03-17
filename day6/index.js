4db23156a8e25c18acb04050588258a09da16eaf
HyperByte <153655587+abrarishere@users.noreply.github.com>
2024-03-17 01:59:47 +0000

    Dayy 6 done

--- day6/index.js
+++ day6/index.js
@@ -1,2 +1,9 @@
-// JavaScript code for Day {day}
-alert("Hello from Day {day}!");
+let radius;
+const PI = 3.14;
+let circumference;
+radius = window.prompt("Enter radius: ");
+radius = Number(radius);
+//PI = 420.90
+
+circumference = 2 * PI * radius;
+console.log("Circumference is: " + circumference);
