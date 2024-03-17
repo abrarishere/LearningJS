a4dc1c22add6ac8cbf5af7ed92461d4482fef24e
HyperByte <153655587+abrarishere@users.noreply.github.com>
2024-03-17 02:13:24 +0000

    D 7 rone

--- day7/index.js
+++ day7/index.js
@@ -1,2 +1,13 @@
-// JavaScript code for Day {day}
-alert("Hello from Day {day}!");
+let x;
+let y = 5;
+let z = 9;
+// x = Math.round(x);
+// x = Math.floor(x);
+// x = Math.ceil(x);
+// x = Math.pow(x, 2);
+// x = Math.sqrt(x);
+// x = Math.abs(x);
+// maximum = Math.max(x, y, z);
+// minimum = Math.min(x, y, z);
+x = Math.PI;
+console.log(x);
