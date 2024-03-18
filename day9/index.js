d60bab4a9a4f663e67bde9b54063500d5d07621c
Muhammad Abrar <legendabrar44@gmail.com>
2024-03-18 15:58:53 +0000

    9vdone

--- day9/index.js
+++ day9/index.js
@@ -1,2 +1,15 @@
-// JavaScript code for Day {day}
-alert("Hello from Day {day}!");
+counter = 0;
+
+document.getElementById("increase").onclick = function increment() {
+  counter++;
+  document.getElementById("counter").innerHTML = counter;
+}
+
+document.getElementById("decrease").onclick = function decrement() {
+  counter--;
+  document.getElementById("counter").innerHTML = counter;
+}
+document.getElementById("reset").onclick = function resetCounter() {
+  counter = 0;
+  document.getElementById("counter").innerHTML = counter;
+}
