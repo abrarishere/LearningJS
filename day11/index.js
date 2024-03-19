76cbe7dc2d5f1fbc18d75963236966e24593cb93
Muhammad Abrar <legendabrar44@gmail.com>
2024-03-19 02:49:57 +0000

    dag 11 done

--- day11/index.js
+++ day11/index.js
@@ -1,2 +1,10 @@
-// JavaScript code for Day {day}
-alert("Hello from Day {day}!");
+// useful string properties
+userName = 'Abrar is here';
+console.log(userName.length);
+console.log(userName.toUpperCase());
+console.log(userName.toLowerCase());
+console.log(userName.indexOf('A'));
+console.log(userName.lastIndexOf('a'));
+console.log(userName.replaceAll('a', 'b'));
+console.log(userName.trim());
+console.log(userName.charAt(0));
