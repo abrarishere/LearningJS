af11157ceedd380a2d093c269004158e65e89f60
Muhammad Abrar <legendabrar44@gmail.com>
2024-03-19 06:59:29 +0000

    dayb14v15

--- day14/index.js
+++ day14/index.js
@@ -1,2 +1,25 @@
-// JavaScript code for Day {day}
-alert("Hello from Day {day}!");
+// if statements in javascript
+
+// let age = 21;
+
+// if (age >= 18) {
+//     console.log("You are an adult");
+// }
+// else if (age <= 0) {
+//     console.log("You haven't been born yet");
+// }
+// else if ( age >= 65) {
+//     console.log("You are a senior");
+// }
+//  else {
+//     console.log("You area children");
+// }
+
+let online = true;
+
+if (online) {
+    console.log("You are online");
+}
+else {
+    console.log("You are offline");
+}
