997bea694d755483325a686859c55dcc43b22390
Muhammad Abrar <legendabrar44@gmail.com>
2024-03-19 07:21:53 +0000

    17

--- day17/index.js
+++ day17/index.js
@@ -1,2 +1,9 @@
-// JavaScript code for Day {day}
-alert("Hello from Day {day}!");
+// and or not operators in javascript
+
+let temp = 10;
+if (temp < 0 && temp >= 30) {
+    console.log("The weather is bad today");
+}
+else if (temp > 30 && temp < 60) {
+    console.log("The weather is moderate today");
+}
