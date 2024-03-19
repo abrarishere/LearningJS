8da0139048e5a35c44c53c37c2cf42e0938f5049
Muhammad Abrar <legendabrar44@gmail.com>
2024-03-19 02:38:47 +0000

    day 10;done

--- day10/index.js
+++ day10/index.js
@@ -1,2 +1,11 @@
-// JavaScript code for Day {day}
-alert("Hello from Day {day}!");
+let a;
+let b;
+let c;
+document.getElementById("roll").onclick = function () {
+    a = Math.floor(Math.random() * 6) + 1;
+    b = Math.floor(Math.random() * 6) + 1;
+    c = Math.floor(Math.random() * 6) + 1;
+    document.getElementById("xLabel").innerHTML = a;
+    document.getElementById("yLabel").innerHTML = b;
+    document.getElementById("zLabel").innerHTML = c
+}
