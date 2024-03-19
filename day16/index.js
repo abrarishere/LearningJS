ee521d087c87376e9e0f1a111bd67de7c467e077
Muhammad Abrar <legendabrar44@gmail.com>
2024-03-19 07:11:05 +0000

    160d

--- day16/index.js
+++ day16/index.js
@@ -1,2 +1,17 @@
-// JavaScript code for Day {day}
-alert("Hello from Day {day}!");
+let grade = 90;
+switch (true) {
+    case grade >= 90:
+        console.log('Excellent');
+        break;
+    case grade >= 80:
+        console.log('Good');
+        break;
+    case grade >= 70:
+        console.log('Fair');
+        break;
+    case grade >= 60:
+        console.log('Poor');
+        break;
+    default:
+        console.log('Invalid Grade');
+}
