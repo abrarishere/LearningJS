fb492245b929d40f3dd71f2e073e421a7bbcadf0
Muhammad Abrar <legendabrar44@gmail.com>
2024-03-19 07:28:15 +0000

    18

--- day18/index.js
+++ day18/index.js
@@ -1,2 +1,18 @@
-// JavaScript code for Day {day}
-alert("Hello from Day {day}!");
+//not
+
+let temp =-15;
+let sunny = true;
+
+if (temp < 0) {
+    console.log("It is cold out");
+}
+else {
+    console.log("It is hot out");
+}
+
+if (!sunny) {
+    console.log("It is cloudy");
+}
+else {
+    console.log("It is sunny");
+}
